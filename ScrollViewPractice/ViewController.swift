ae0d382f657cef11833d8ce727a7fc6038663795
Ekaterina Pecherskaya <Pechorskaya.K8@gmail.com>
2024-01-06 14:34:19 +0200

    Initial Commit

---
+++ ScrollViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollViewPractice
+//
+//  Created by Chorrs on 6.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
